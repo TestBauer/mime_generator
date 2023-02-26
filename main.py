5ed3e32aa613d57d2434672a584ea17d43a16a7f
Christian Bauer <52620869+TestBauer@users.noreply.github.com>
2023-02-26 10:43:54 +0000

    added new files

---
+++ main.py
@@ -0,0 +1,48 @@
+import json
+from textwrap import wrap
+
+
+def getListofNumArrayfromHeader(header_signs: str) -> list:
+    num = None
+    r2 = []
+    for heads in header_signs:
+        # TODO: Handle offset value from Signs
+        num = (wrap(heads.split(",")[1], 2))
+        r = []
+        for i in num:
+            a = ('0x' + str(i))
+            a = int(a, 16)
+            r.append(a)
+        r2.append(r)
+    return r2
+
+
+def getHeaderbyExtension(extension: str, magiclist: json) -> object:
+    r_list = {}
+    try:
+        r_list = magiclist[extension.lower()]
+
+    except KeyError:
+        print(f"Unknown Extension : {extension} !")
+
+    finally:
+
+        return r_list
+
+
+if __name__ == "__main__":
+    # // https://gist.github.com/qti3e/6341245314bf3513abb080677cd1c93b#file-extensions-json
+    f = open("./config/magic.list.json")
+    magicl = json.load(f)
+
+    ext = "mpeg"
+    ext2 = "." + ext
+    header = getHeaderbyExtension(extension=ext, magiclist=magicl)
+    if header is not []:
+        list_of_num = getListofNumArrayfromHeader(header["signs"])
+
+        for count, num in enumerate(list_of_num):
+            f = open(f"./output/file_{ext}_{count}{ext2}", "wb")
+            arr = bytearray(num)
+            f.write(arr)
+            f.close()
